3fd3cba9f572590ff35a818474b608e39644af9c
Danielle Priscilla <daniellepr27@gmail.com>
2026-07-28 19:58:27 +0700

    setelah menambahkan models

---
+++ app/Filament/Admin/Resources/Items/Schemas/ItemForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Items\Schemas;
+
+use Filament\Schemas\Schema;
+
+class ItemForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
